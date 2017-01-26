c5b7b6a69ec8f2b745ad2b706cf4ef159d2dbe99
Balu <balujntu2012@gmail.com>
2017-01-26 15:11:00 +0530

    gulp file updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
+"use strict";
 var gulp =  require('gulp');
-gulp.task('hello',function(){
-	console.log("hello gulp");
-});
-var sass = require('gulp-sass');
-gulp.task("sass",function(){
+var sass =  require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task("browserSync",function(){
+	browserSync.init({
+		server : {
+			baseDir : 'app'
+		}
+	})
+})
+gulp.task("styles",function(){
 	return gulp.src('app/scss/**/*.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({
+      stream: true
+    }))
 });
+
+gulp.task("watch",['browserSync', 'styles'],function(){
+	gulp.watch('app/scss/**/*.scss',['styles']); 
+	gulp.watch('app/*.html',browserSync.reload);
+	gulp.watch('app/js/*.js',browserSync.reload);
+})
+
